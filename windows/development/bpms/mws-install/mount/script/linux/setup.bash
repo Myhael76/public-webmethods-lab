0219f127ac0651fe5ca2bab753f01a47fcc2d9eb
miun <mihai.ungureanu@softwareag.com>
2020-07-01 09:43:34 +0200

    Leave out the runs results from git

--- windows/development/bpms/mws-install/mount/script/linux/setup.bash
+++ windows/development/bpms/mws-install/mount/script/linux/setup.bash
@@ -2,26 +2,28 @@
 
 CRT_INSTALL_DATE=`date +%y-%m-%dT%H.%M.%S`
 
-mkdir -p /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}
+RUN_FOLDER=/mnt/wm-install-mws-mount/runs/install_${CRT_INSTALL_DATE}
+
+mkdir -p ${RUN_FOLDER}
 
 pushd .
 
-cd /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}
+cd ${RUN_FOLDER}
 
 ###### 01 - install
 
 /mnt/wm-install-files/installer.bin \
  -readScript /mnt/wm-install-mws-mount/script/linux/wm/install.wmscript \
  -debugLvl verbose \
- -debugFile /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/thisInstallation.log \
- > /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/01-install.out \
- 2> /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/01-install.err
+ -debugFile ${RUN_FOLDER}/01-install.log \
+ > ${RUN_FOLDER}/01-install.out \
+ 2> ${RUN_FOLDER}/01-install.err
 
 ###### 02 - boot spm
 
 /mnt/wm-install-files/sum-bootstrap.bin --accept-license -d /opt/sagsum \
- > /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/02-sum-boot.out \
- 2> /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/02-sum-boot.err
+ > ${RUN_FOLDER}/02-sum-boot.out \
+ 2> ${RUN_FOLDER}/02-sum-boot.err
  
 ###### 03 - copy files
 
@@ -36,8 +38,8 @@ cp /mnt/wm-install-mws-mount/script/linux/wm/fixes.wmscript.txt /dev/shm/fixes.w
 cat /mnt/wm-install-base-mount/secret/empower-cred-fixes.txt >> /dev/shm/fixes.wmscript.txt
 
 ./UpdateManagerCMD.sh -readScript /dev/shm/fixes.wmscript.txt \
- > /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/04-patching.out \
- 2> /mnt/wm-install-mws-mount/install_${CRT_INSTALL_DATE}/04-patching.err 
+ > ${RUN_FOLDER}/04-patching.out \
+ 2> ${RUN_FOLDER}/04-patching.err 
 
 rm /dev/shm/fixes.wmscript.txt
 
