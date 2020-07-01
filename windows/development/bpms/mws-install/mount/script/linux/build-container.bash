5663b2c775bd5f0166a594d0844e70926aab15d8
miun <mihai.ungureanu@softwareag.com>
2020-07-01 11:44:18 +0200

    MWS part #2 after instance creation bug workaround

---
+++ windows/development/bpms/mws-install/mount/script/linux/build-container.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 01 - Cleanup original folder
+
+# TODO: optimize, for the moment I found this to be more appropriate than selecting contetn in Dockerfile
+
+rm -rf /opt/softwareag/_documentation
+rm -rf /opt/softwareag/bin
+rm -rf /opt/softwareag/common/src
+rm -rf /opt/softwareag/profiles/SPM         # not useful in docker
+rm -rf /opt/softwareag/jvm/*.bck            # no backup needed
+rm -rf /opt/softwareag/jvm/jvm/src.zip
+rm -rf /opt/softwareag/jvm/jvm/demo
+rm -rf /opt/softwareag/jvm/jvm/man
+rm -rf /opt/softwareag/jvm/jvm/sample
+
+find /opt/softwareag -type f -iname "*.pdf" -delete
+
+
+# 02 - Copy the extra files 
+
+unalias cp #it is aliased to cp -i
+cp -r /mnt/wm-install-mws-mount/wms_extra_files/* /opt/softwareag/
+cp /mnt/wm-install-base-libs/mysql-connector-java-8.0.15.jar /opt/softwareag/MWS/lib/
+
+# TODO: Continue
--- windows/development/bpms/mws-install/mount/script/linux/setup.bash
+++ windows/development/bpms/mws-install/mount/script/linux/setup.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This script installs MWS in /opt/softwareag with BPMS related UIs and patches it to the latest version in online mode
+# No MWS instance is created at install time
+# Must have access to onine Software AG fix servers
+# TODO
+# - does not consider proxy needs for the moment. Add subsequently if necessary
+# Prerequisites
+# - centos-wm-install-helper docker image must exist. build and test it before using this project, which uses the names defined there
+# - the image declared in the set-env for the above project must contain all the products in wm/install.wmscript
+
 CRT_INSTALL_DATE=`date +%y-%m-%dT%H.%M.%S`
 
 RUN_FOLDER=/mnt/wm-install-mws-mount/runs/install_${CRT_INSTALL_DATE}
@@ -11,7 +20,7 @@ pushd .
 cd ${RUN_FOLDER}
 
 ###### 01 - install
-
+echo `date +%y-%m-%dT%H.%M.%S_%3N`" - 01 - Installing product ..."
 /mnt/wm-install-files/installer.bin \
  -readScript /mnt/wm-install-mws-mount/script/linux/wm/install.wmscript \
  -debugLvl verbose \
@@ -20,27 +29,24 @@ cd ${RUN_FOLDER}
  2> ${RUN_FOLDER}/01-install.err
 
 ###### 02 - boot spm
+echo `date +%y-%m-%dT%H.%M.%S_%3N`" - 02 - Bootstrapping SPM ..."
 
 /mnt/wm-install-files/sum-bootstrap.bin --accept-license -d /opt/sagsum \
  > ${RUN_FOLDER}/02-sum-boot.out \
  2> ${RUN_FOLDER}/02-sum-boot.err
- 
-###### 03 - copy files
 
-cp /mnt/wm-install-base-libs/mysql*.jar /opt/softwareag/MWS/lib/
-cp /mnt/wm-install-mws-mount/lib/* /opt/softwareag/MWS/lib/
-
-###### 04 - Patch installation
- 
+###### 03 - Patch installation
+echo `date +%y-%m-%dT%H.%M.%S_%3N`" - 03 - Applying latest fixes ..."
 cd /opt/sagsum/bin/
 
 cp /mnt/wm-install-mws-mount/script/linux/wm/fixes.wmscript.txt /dev/shm/fixes.wmscript.txt
 cat /mnt/wm-install-base-mount/secret/empower-cred-fixes.txt >> /dev/shm/fixes.wmscript.txt
 
 ./UpdateManagerCMD.sh -readScript /dev/shm/fixes.wmscript.txt \
- > ${RUN_FOLDER}/04-patching.out \
- 2> ${RUN_FOLDER}/04-patching.err 
+ > ${RUN_FOLDER}/03-patching.out \
+ 2> ${RUN_FOLDER}/03-patching.err 
 
 rm /dev/shm/fixes.wmscript.txt
 
+echo `date +%y-%m-%dT%H.%M.%S_%3N`" - Finished!"
 popd .
