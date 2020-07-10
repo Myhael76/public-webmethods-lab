c0c0021dbd76084c1d0b110a72283d6e5a783935
Mihai UNGUREANU <mihai.ungureanu-external@rbinternational.com>
2020-07-10 17:27:17 +0200

    Small fix for pullContent

--- windows/pullContent.bat
+++ windows/pullContent.bat
@@ -1,3 +1,6 @@
+.\..\common\mount\extra\lib\ext\ goto download
+mkdir .\..\common\mount\extra\lib\ext\
+:download
 if exist .\..\common\mount\extra\lib\ext\mysql-connector-java-8.0.15.jar  goto end
 powershell -Command^
  "Invoke-WebRequest https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.15/mysql-connector-java-8.0.15.jar -OutFile .\..\common\mount\extra\lib\ext\mysql-connector-java-8.0.15.jar"
