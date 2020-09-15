c3f8a630e9aea9f60287b7c5608b94da81325c26
miun <mihai.ungureanu@softwareag.com>
2020-09-15 16:15:10 +0200

    Example for unattended fix image creation

---
+++ projects/fix-img-for-inventory-example/scripts/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+. /mnt/scripts/lib/common.sh
+
+logI "Boostrapping SUM"
+bootstrapSum
+
+logI "Preparing script"
+
+cp /mnt/wm-files/patch.wmscript "${WMLAB_RUN_FOLDER}"
+
+cat /mnt/wm-files/sum-online-credentials.txt >> "${WMLAB_RUN_FOLDER}/patch.wmscript"
+
+cd /opt/sag/sum/bin
+
+./UpdateManagerCMD.sh \
+    -readScript "${WMLAB_RUN_FOLDER}/patch.wmscript" \
+    -installDir /mnt/wm-files/inventory.json \
+    -createImage "${WMLAB_RUN_FOLDER}/fixes.image" \
+    > "${WMLAB_RUN_FOLDER}/Update.out" \
+    2> "${WMLAB_RUN_FOLDER}/Update.err"
+
+logI "Pausing for debug"
+
+tail -f /dev/null
+
+
