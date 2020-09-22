c3c0f1aeeddd6b8870a99fefcb45cfd2363ed8b2
miun <mihai.ungureanu@softwareag.com>
2020-09-22 19:11:02 +0200

    Rename install helper run project

--- projects/centos-wm-install-helper/02-up.v1007_IST.bat
+++
@@ -1,8 +0,0 @@
-set H_WMLAB_PRODUCTS_VERSION=1007
-set H_WMLAB_INSTALLER_BIN=f:\k\SAG\Installers\LNX64\ISTOct2020\SoftwareAGInstaller-Linux_x86_64_IST_v20200903_1.bin
-set H_WMLAB_TAKE_SNAPHOTS=1
-set H_WMLAB_SKIP_PATCHING=1
-
-docker-compose up
-
-pause
