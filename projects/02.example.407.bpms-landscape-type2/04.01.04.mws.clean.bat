241909adfebf7629ba1296859b920dbf845d5f67
miun <mihai.ungureanu@softwareag.com>
2020-10-02 14:02:12 +0200

    Default not to take snapshots, small corrections

--- projects/02.example.407.bpms-landscape-type2/04.01.04.mws.clean.bat
+++ projects/02.example.407.bpms-landscape-type2/04.01.04.mws.clean.bat
@@ -4,4 +4,4 @@ echo This will clean the mws installation! Continue?
 
 pause
 
-docker-compose -f .\03.01.docker-compose_mws_setup.yml down -v
+docker-compose -f .\04.01.docker-compose_mws_setup.yml down -v
