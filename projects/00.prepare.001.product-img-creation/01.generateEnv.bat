a54256c9c20a86109510087ae920170bf454d264
myhael76 <myhael76.noreply@nowhere.com>
2021-02-12 22:04:49 +0100

    Config simplification after tests

--- projects/00.prepare.001.product-img-creation/01.generateEnv.bat
+++ projects/00.prepare.001.product-img-creation/01.generateEnv.bat
@@ -1,24 +1,8 @@
 @echo off
 
-:: this project depends on the .env from project 00 and 001.build.000
-
-:: take .env from 00
-if exist ..\00.commons\.env goto copy_00_env
-
-PUSHD .
-cd ..\00.commons
-echo generating .env file for project 00.commons
-call generateEnv.bat
-POPD
-
-:copy_00_env
-copy ..\00.commons\.env .
-
-:: copy local env file
-echo . >>.\.env
-type .\.env_base.md >> .\.env
+copy .\.env_base.md .\.env
 
 echo . >>.\.env
-echo ## Local files and folders >>.\.env
+echo ## Local files and folders for project 00.prepare.001.product-img-creation >>.\.env
 
 Powershell.exe -executionpolicy Bypass -File .\powershell\generateEnvFile.ps1
